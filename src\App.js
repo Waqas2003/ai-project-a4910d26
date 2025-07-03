1e36a7b6b25cdb617dcc3e86e0c498150675b892
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-03 19:19:09 +0500

    Add src\App.js

---
+++ src\App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HeroSection from './components/HeroSection';
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-100">
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
